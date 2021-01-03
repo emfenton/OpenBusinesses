4c10ffc28d48260cd6bce0c0bc96cf84f54f45fd
Erin <erinmfenton@gmail.com>
2021-01-03 13:22:14 -0800

    changed the site verbiage and added a column to the business model

---
+++ Migrations/20210103211629_AddingModelColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesBusiness.Migrations
+{
+    public partial class AddingModelColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Business",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Business");
+        }
+    }
+}
